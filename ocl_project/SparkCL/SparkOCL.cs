22e313581a1f9708a86e790e33b5f7bdbf905979
VBB <librusekus35790@gmail.com>
2024-11-17 00:18:24 +0700

    Разъединить SparkCL и SparkOCL

---
+++ ocl_project/SparkCL/SparkOCL.cs
@@ -0,0 +1,633 @@
+using Silk.NET.OpenCL;
+using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using static SparkOCL.CLHandle;
+
+// обёртка над Silk.NET.OpenCL для удобного использования в csharp
+namespace SparkOCL
+{
+    internal static class CLHandle
+    {
+        static public CL OCL = CL.GetApi();
+    }
+
+    public class Event
+    {
+        public nint Handle { get; }
+
+        unsafe public ulong GetProfilingInfo(
+            ProfilingInfo info
+        )
+        {
+            ulong time;
+            int err = OCL.GetEventProfilingInfo(Handle, info, 8, &time, null);
+
+            if (err != (int)ErrorCodes.Success)
+            {
+                throw new System.Exception($"Couldn't get profiling info, code: {err}");
+            }
+
+            return time;
+        }
+        
+        unsafe public void Wait()
+        {
+
+            var handle = Handle;
+            int err = OCL.WaitForEvents(1, &handle);
+            
+            if (err != (int)ErrorCodes.Success)
+            {
+                throw new System.Exception($"Couldn't wait for event, code: {err}");
+            }
+        }
+
+        internal Event(nint h)
+        {
+            Handle = h;
+        }
+
+        ~Event()
+        {
+            OCL.ReleaseEvent(Handle);
+        }
+    }
+
+    class Context
+    {
+        public nint Handle { get; }
+
+        unsafe static public Context FromDevice(
+            Device device)
+        {
+            var device_h = device.Handle;
+            int err_p;
+            var h = OCL.CreateContext(null, 1, &device_h, null, null, &err_p);
+
+            if (err_p != (int)ErrorCodes.Success)
+            {
+                throw new System.Exception($"Couldn't create context on requested device, code: {err_p}");
+            }
+
+            var res = new Context(h);
+            return res;
+        }
+
+        unsafe static public Context FromType(
+            DeviceType type)
+        {
+
+            var platforms = new List<Platform>();
+            Platform.Get(out platforms);
+
+            // Next, create an OpenCL context on the platform.  Attempt to
+            // create a GPU-based context, and if that fails, try to create
+            // a CPU-based context.
+            nint[] contextProperties =
+            [
+                (nint)ContextProperties.Platform,
+                platforms[0].Handle,
+                0
+            ];
+
+            fixed (nint* p = contextProperties)
+            {
+                int errNum;
+                var context_handle = OCL.CreateContextFromType(p, DeviceType.Gpu, null, null, out errNum);
+                if (errNum != (int) ErrorCodes.Success)
+                {
+                    throw new System.Exception($"Couldn't create context on requested device type, code: {errNum}");
+                }
+
+                return new Context(context_handle);
+            }
+        }
+
+        private Context(nint h)
+        {
+            Handle = h;
+        }
+
+        ~Context()
+        {
+            CLHandle.OCL.ReleaseContext(Handle);
+        }
+    }
+
+    class Platform
+    {
+        public nint Handle { get; }
+        unsafe public static void Get(out List<Platform> platforms)
+        {
+            uint n = 0;
+            var err = OCL.GetPlatformIDs(0, null, &n);
+            if (err != (int) ErrorCodes.Success)
+            {
+                throw new System.Exception($"Couldn't get platform ids, code: {err}");
+            }
+
+            var ids = new nint[n];
+            err = OCL.GetPlatformIDs(n, ids, (uint *)null);
+            if (err != (int) ErrorCodes.Success)
+            {
+                throw new System.Exception($"Couldn't get platform ids, code: {err}");
+            }
+
+            platforms = new()
+            {
+                Capacity = (int)n
+            };
+            for (int i = 0; i < n; i++)
+            {
+                var p = new Platform(ids[i]);
+                platforms.Add(p);
+            }
+        }
+
+        unsafe public void GetDevices(DeviceType type, out List<Device> devices)
+        {
+            uint n = 0;
+            var err = OCL.GetDeviceIDs(Handle, type, 0, null, &n);
+            if (err != (int) ErrorCodes.Success)
+            {
+                throw new System.Exception($"Couldn't get devices ID, code: {err}");
+            }
+
+            var ids = new nint[n];
+            err = OCL.GetDeviceIDs(Handle, type, n, ids, (uint *)null);
+            if (err != (int) ErrorCodes.Success)
+            {
+                throw new System.Exception($"Couldn't get devices ID, code: {err}");
+            }
+
+            devices = new((int) n);
+            for (int i = 0; i < n; i++)
+            {
+                var p = new Device(ids[i]);
+                devices.Add(p);
+            }
+        }
+
+        private Platform(nint h)
+        {
+            Handle = h;
+        }
+    }
+
+    class Device
+    {
+        DeviceType Type { get; }
+        public nint Handle { get; }
+
+        internal Device(nint h)
+        {
+            Handle = h;
+        }
+
+        ~Device()
+        {
+            CLHandle.OCL.ReleaseDevice(Handle);
+        }
+    }
+
+
+    class NDRange
+    {
+        public uint Dimensions { get; }
+        public nuint[] Sizes { get; } = [1, 1, 1];
+
+        public NDRange()
+        {
+            Dimensions = 0;
+            Sizes[0] = 0;
+            Sizes[1] = 0;
+            Sizes[2] = 0;
+        }
+        public NDRange(nuint size0)
+        {
+            Dimensions = 1;
+            Sizes[0] = size0;
+            Sizes[1] = 1;
+            Sizes[2] = 1;
+        }
+        public NDRange(
+            nuint size0,
+            nuint size1)
+        {
+            Dimensions = 2;
+            Sizes[0] = size0;
+            Sizes[1] = size1;
+            Sizes[2] = 1;
+        }
+        public NDRange(
+            nuint size0,
+            nuint size1,
+            nuint size2)
+        {
+            Dimensions = 1;
+            Sizes[0] = size0;
+            Sizes[1] = size1;
+            Sizes[2] = size2;
+        }
+
+        nuint this[int i]
+        {
+            get => Sizes[i];
+        }
+    }
+
+    class CommandQueue
+    {
+        public nint Handle { get; }
+
+        public unsafe CommandQueue(Context context, Device device)
+        {
+
+            int err;
+            QueueProperties[] props = [
+                (QueueProperties)CommandQueueInfo.Properties, (QueueProperties) CommandQueueProperties.ProfilingEnable,
+                0
+            ];
+            fixed (QueueProperties *p = props)
+            {
+                Handle = OCL.CreateCommandQueueWithProperties(context.Handle, device.Handle, p, &err);
+            }
+
+            if (err != (int)ErrorCodes.Success)
+            {
+                throw new System.Exception($"Couldn't create command queue, code: {err}");
+            }
+        }
+
+        public void Finish()
+        {
+            int err = CLHandle.OCL.Finish(Handle);
+
+            if (err != (int)ErrorCodes.Success)
+            {
+                throw new System.Exception($"Couldn't finish command queue, code: {err}");
+            }
+        }
+
+        public unsafe void EnqueueNDRangeKernel(
+            Kernel kernel,
+            NDRange offset,
+            NDRange global,
+            NDRange local,
+            out Event @event)
+        {
+
+            int err;
+            nint event_h;
+            fixed (nuint *g = global.Sizes)
+            fixed (nuint *o = offset.Sizes)
+            {
+                err = OCL.EnqueueNdrangeKernel(
+                    Handle,
+                    kernel.Handle,
+                    global.Dimensions,
+                    offset.Dimensions != 0 ? o : null,
+                    g,
+                    null,
+                    0,
+                    null,
+                    &event_h);
+            }
+            @event = new Event(event_h);
+
+            if (err != (int)ErrorCodes.Success)
+            {
+                throw new System.Exception($"Couldn't enqueue kernel, code: {err}");
+            }
+        }
+
+        public unsafe void* EnqueueMapBuffer<T>(
+            Buffer<T> buffer,
+            bool blocking,
+            MapFlags flags,
+            nuint offset,
+            nuint count,
+            out Event @event)
+        where T : unmanaged
+        {
+            nint event_h;
+            var ptr = OCL.EnqueueMapBuffer(
+                Handle,
+                buffer.Handle,
+                blocking,
+                flags,
+                offset,
+                count * (nuint) sizeof(T),
+                0,
+                null,
+                out event_h,
+                out int err);
+
+            if (err != (int) ErrorCodes.Success)
+            {
+                throw new System.Exception($"Couldn't enqueue buffer map, code: {err}");
+            }
+            @event = new Event(event_h);
+
+            return ptr;
+        }
+
+        public unsafe void EnqueueReadBuffer<T>(
+            Buffer<T> buffer,
+            bool blocking,
+            nuint offset,
+            Array<T> array,
+            out Event @event)
+        where T : unmanaged
+        {
+
+            nint event_h;
+            int err = OCL.EnqueueReadBuffer(
+                Handle,
+                buffer.Handle,
+                blocking,
+                offset,
+                array.Count * (nuint) sizeof(T),
+                array.Buf,
+                0,
+                null,
+                out event_h);
+
+            if (err != (int) ErrorCodes.Success)
+            {
+                throw new System.Exception($"Couldn't enqueue buffer read, code: {err}");
+            }
+            @event = new Event(event_h);
+        }
+
+        public unsafe void EnqueueWriteBuffer<T>(
+            Buffer<T> buffer,
+            bool blocking,
+            nuint offset,
+            Array<T> array,
+            out Event @event)
+        where T : unmanaged
+        {
+
+            nint event_h;
+            int err = OCL.EnqueueWriteBuffer(
+                Handle,
+                buffer.Handle,
+                blocking,
+                offset,
+                array.Count * (nuint) sizeof(T),
+                array.Buf,
+                0,
+                null,
+                out event_h);
+
+            if (err != (int) ErrorCodes.Success)
+            {
+                throw new System.Exception($"Couldn't enqueue buffer read, code: {err}");
+            }
+            @event = new Event(event_h);
+        }
+
+        public unsafe void EnqueueUnmapMemObject<T>(
+            Buffer<T> buffer,
+            void *ptr,
+            out Event @event)
+        where T : unmanaged
+        {
+
+            nint event_h;
+            int err = OCL.EnqueueUnmapMemObject(
+                Handle,
+                buffer.Handle,
+                ptr,
+                0,
+                null,
+                out event_h);
+
+            if (err != (int) ErrorCodes.Success)
+            {
+                throw new System.Exception($"Couldn't enqueue memory object unmap, code: {err}");
+            }
+            @event = new Event(event_h);
+        }
+
+        private CommandQueue(nint h)
+        {
+            Handle = h;
+        }
+
+        ~CommandQueue()
+        {
+            CLHandle.OCL.ReleaseCommandQueue(Handle);
+        }
+    }
+
+    class Kernel
+    {
+        public nint Handle { get; }
+
+        unsafe public Kernel(
+            Program program,
+            string name)
+        {
+            int err;
+            Handle = OCL.CreateKernel(program.Handle, Encoding.ASCII.GetBytes(name), &err);
+
+            if (err != (int) ErrorCodes.Success)
+            {
+                throw new System.Exception($"Failed to create kernel, code: {err}");
+            }
+        }
+
+        ~Kernel()
+        {
+            CLHandle.OCL.ReleaseKernel(Handle);
+        }
+
+        unsafe public void SetArg<T>(
+            uint arg_index,
+            SparkOCL.Buffer<T> buffer)
+        where T : unmanaged, INumber<T>
+        {
+            var binding = buffer.Handle;
+
+            int err = OCL.SetKernelArg(Handle, arg_index, (nuint)sizeof(nint), ref binding);
+            if (err != (int) ErrorCodes.Success)
+            {
+                throw new System.Exception($"Failed to set kernel argument (index = {arg_index}), code: {err}");
+            }
+        }
+
+        unsafe public void SetArg<T>(
+            uint arg_index,
+            T arg)
+        where T: unmanaged
+        {
+
+            int err = OCL.SetKernelArg(Handle, arg_index, (nuint)sizeof(T), ref arg);
+            if (err != (int) ErrorCodes.Success)
+            {
+                throw new System.Exception($"Failed to set kernel argument, code: {err}, arg size: {(nuint)sizeof(T)}");
+            }
+        }
+
+        unsafe public void SetSize(
+            uint arg_index,
+            nuint sz)
+        {
+
+            int err = OCL.SetKernelArg(Handle, arg_index, (nuint)sizeof(float) * sz, null);
+            if (err != (int)ErrorCodes.Success)
+            {
+                throw new System.Exception($"Failed to set kernel argument, code: {err}");
+            }
+        }
+    }
+
+    class Buffer<T>
+    where T : unmanaged
+    {
+        public nint Handle { get; }
+
+        unsafe public Buffer(Context context, MemFlags flags, SparkOCL.Array<T> array)
+        {
+            int err;
+            Handle = OCL.CreateBuffer(context.Handle, flags, (nuint) sizeof(T) * array.Count, array.Buf, &err);
+            if (err != (int) ErrorCodes.Success)
+            {
+                throw new System.Exception($"Failed to create buffer, code: {err}");
+            }
+        }
+
+        ~Buffer()
+        {
+            CLHandle.OCL.ReleaseMemObject(Handle);
+        }
+    }
+
+    unsafe class Array<T> : IDisposable
+    where T: unmanaged
+    {
+        public T* Buf { get; internal set; }
+        public nuint Count { get; }
+        public nuint ElementSize { get; }
+
+        public Array (ReadOnlySpan<T> array)
+        {
+            ElementSize = (nuint)sizeof(T);
+            Buf = (T*)NativeMemory.AlignedAlloc((nuint)array.Length * ElementSize, 4096);
+            this.Count = (nuint)array.Length;
+            array.CopyTo(new Span<T>(Buf, array.Length));
+        }
+
+        public Array (nuint size)
+        {
+            ElementSize = (nuint)sizeof(T);
+            Buf = (T*)NativeMemory.AlignedAlloc(size * ElementSize, 4096);
+            this.Count = size;
+        }
+
+        public T this[int i]
+        {
+            get
+            {
+                return Buf[i];
+            }
+            set
+            {
+                Buf[i] = value;
+            }
+        }
+
+
+        private bool disposedValue;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                }
+
+                NativeMemory.AlignedFree(Buf);
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        ~Array()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: false);
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+
+    class Program
+    {
+        internal nint Handle { get; }
+
+        unsafe static public Program FromFilename(
+            Context context,
+            Device device,
+            string fileName)
+        {
+            using StreamReader sr = new StreamReader(fileName);
+            string clStr = sr.ReadToEnd();
+
+            int err;
+            var program = OCL.CreateProgramWithSource(context.Handle, 1, new string[] { clStr }, null, &err);
+            if (program == IntPtr.Zero || err != (int) ErrorCodes.Success)
+            {
+                throw new System.Exception($"Failed to create CL program from source, code: {err}");
+            }
+
+            var errNum = OCL.BuildProgram(program, 0, null, (byte*)null, null, null);
+
+            if (errNum != (int)ErrorCodes.Success)
+            {
+                _ = OCL.GetProgramBuildInfo(program, device.Handle, ProgramBuildInfo.BuildLog, 0, null, out nuint buildLogSize);
+                byte[] log = new byte[buildLogSize / (nuint)sizeof(byte)];
+                fixed (void* pValue = log)
+                {
+                    OCL.GetProgramBuildInfo(program, device.Handle, ProgramBuildInfo.BuildLog, buildLogSize, pValue, null);
+                }
+                string? build_log = System.Text.Encoding.UTF8.GetString(log);
+
+                //Console.WriteLine("Error in kernel: ");
+                Console.WriteLine("=============== OpenCL Program Build Info ================");
+                Console.WriteLine(build_log);
+                Console.WriteLine("==========================================================");
+
+                OCL.ReleaseProgram(program);
+                throw new Exception($"OpenCL build failed.");
+            }
+
+            return new Program(program);
+        }
+
+        Program(nint h)
+        {
+            Handle = h;
+        }
+
+        ~Program()
+        {
+            CLHandle.OCL.ReleaseProgram(Handle);
+        }
+    }
+}
